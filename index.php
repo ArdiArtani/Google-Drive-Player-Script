0238ea7ec77a14ba430e1cd4b85479b96d9fc9b0
Ardi <ardiasllani@hotmail.com>
2017-06-02 12:35:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,60 @@
 <?php
-error_reporting(E_ERROR | E_PARSE);
-if(!function_exists('curl1'))
-require_once dirname(__FILE__).'/curl.class.php';
-function Drive($link) {
-	$url = urldecode($link);
-	$get = curl1($url);
+error_reporting(0);
+include "curl_gd.php";
 
-	$data = explode(',["fmt_stream_map","', $get);
-	$data = explode('"]', $data[1]);
-	$data = str_replace(array('\u003d', '\u0026'), array('=', '&'), $data[0]);
-	$data = explode(',', $data);
-	asort($data);
-	foreach($data as $list) {
-		$data2 = explode('|', $list);
-		if($data2[0] == 37) {$q1080p	.= '1080p=>'.preg_replace("/\/[^\/]+\.google\.com/","/redirector.googlevideo.com",$data2[1]).'|';}
-		if($data2[0] == 22) {$q720p	.= '720p=>'.preg_replace("/\/[^\/]+\.google\.com/","/redirector.googlevideo.com",$data2[1]).'|';}
-		if($data2[0] == 59) {$q480p	.= '480p=>'.preg_replace("/\/[^\/]+\.google\.com/","/redirector.googlevideo.com",$data2[1]).'|';}
-		if($data2[0] == 18) {$q360p	.= '360p=>'.preg_replace("/\/[^\/]+\.google\.com/","/redirector.googlevideo.com",$data2[1]).'|';}
-	}
-	$js = $q1080p.'<br />';
-	$js .= $q720p.'<br />';
-	$js .= $q480p.'<br />';
-	$js .= $q360p.'<br />';
-	return rtrim($js, '|<br />');
+if($_POST['submit'] != ""){
+	$url = $_POST['url'];
+	$tmp = explode("file/d/",$url);
+	$tmp2 = explode("/",$tmp[1]);
+	$id = $tmp2[0];
+	$linkdown = trim(getlink($id));
 }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+	<title>Get link Google Drive</title>
+</head>
+<body>
 
-echo Drive('https://drive.google.com/file/d/0B_gSLjUc_vsjcDNLd3dwNHJ3cHM/preview');
+	<!-- Styles -->
+  <link rel="stylesheet" href="https://cdn.plyr.io/2.0.13/plyr.css">
+
+  <!-- Docs styles -->
+  <link rel="stylesheet" href="https://cdn.plyr.io/2.0.13/demo.css">
+	<style>
+		.container {
+		  max-width: 800px;
+		  margin: 0 auto;
+		}
+		.plyr {
+		  border-radius: 4px;
+		  margin-bottom: 15px;
+		}
+	</style>
+
+	<div class="container">
+		<form action="" method="POST">
+			<input type="text" size="80" name="url" value="https://drive.google.com/file/d/0ByaRd0R0Qyatcmw2dVhQS0NDU0U/view"/>
+			<input type="submit" value="GET" name="submit" />
+		</form>
+		<br />
+
+	  <video controls crossorigin>
+	    <!-- Video files -->
+	    <source src="<?php echo $linkdown;?>" type="video/mp4">
+
+	    <!-- Fallback for browsers that don't support the <video> element -->
+	    <a href="<?php echo $linkdown;?>" download>Download</a>
+	  </video>
+	</div>
+
+	<!-- Plyr core script -->
+ <script src="https://cdn.plyr.io/2.0.13/plyr.js"></script>
+
+ <!-- Docs script -->
+ <script src="https://cdn.plyr.io/2.0.13/demo.js"></script>
+
+</body>
+</html>
